752ef3575004f4cdcbd7e1ea1bf1af3f3d7d2a69
Ata-92 <atakal92@gmail.com>
2021-06-02 14:23:10 +0300

    Style Container and Wrapper

---
+++ src/components/about/AboutStyle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #00ADB5;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  border: .1vw solid #fff;
+  border-radius: .3vw;
+  padding: 1vh 1vw;
+`
