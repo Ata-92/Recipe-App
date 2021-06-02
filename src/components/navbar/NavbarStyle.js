d4de7a5fca9ba7c0bb1e5511a42d6ad764ac9285
Ata-92 <atakal92@gmail.com>
2021-06-02 14:37:37 +0300

    Style Nav component

--- src/components/navbar/NavbarStyle.js
+++ src/components/navbar/NavbarStyle.js
@@ -28,3 +28,55 @@ export const Container = styled.div`
     }
   }
 `;
+
+export const Nav = styled.nav`
+  width: 50vw;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  & a {
+    color: #00ADB5;
+    font-weight: bolder;
+    font-size: x-large;
+    text-decoration: none;
+    transition: all 0.3s ease-in-out;
+    font-family: 'Times New Roman', Times, serif;
+
+    & span {
+      font-family: 'Times New Roman', Times, serif;
+      font-size: xx-large;
+    }
+
+    &:hover {
+      color: aqua;
+      font-weight: bold;
+      transition: all 0.3s ease-in-out;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    ${props =>
+      props.hamburger
+        ? css`
+            position: absolute;
+            top: 8vh;
+            left: 0;
+            right: 0;
+            display: flex;
+            flex-direction: column;
+            background-color: aliceblue;
+            width: 100%;
+            justify-content: center;
+            align-items: center;
+          `
+        : css`
+            display: none;
+          `}
+
+    & a {
+      display: block;
+      margin-bottom: 3vh;
+    }
+  }
+`;
