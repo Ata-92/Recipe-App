1b4e088bbfea9906d54e91b19319af3f499c5892
Ata-92 <atakal92@gmail.com>
2021-06-02 14:24:57 +0300

    Import components and an image

---
+++ src/components/details/Details.js
@@ -0,0 +1,22 @@
+import {
+  Container,
+  Heading,
+  H2,
+  Img,
+  LeftInfo,
+  Li,
+  Ol,
+  P,
+  RecipeImage,
+  RightInfo,
+  Wrapper,
+} from "./DetailsStyle";
+import diet from "../../assets/diet.svg";
+
+const Details = (props) => {
+
+  return (
+  );
+};
+
+export default Details;
