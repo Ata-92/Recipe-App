058759c0c6771cb21f9974824deed07c0b5a21a2
Ata-92 <atakal92@gmail.com>
2021-06-02 14:04:41 +0300

    Add return

--- src/components/recipe/RecipeCard.js
+++ src/components/recipe/RecipeCard.js
@@ -3,6 +3,11 @@ import { useHistory } from 'react-router-dom';
 
 const RecipeCard = ({ card }) => {
   return (
+    <Card id={card.id}>
+      <H5>{card.recipe.label}</H5>
+      <Img src={card.recipe.image} alt={card.recipe.label} onError={handleError}/>
+      <Button onClick={() => handleDetails(card)}>View More</Button>
+    </Card>
   );
 };
 
