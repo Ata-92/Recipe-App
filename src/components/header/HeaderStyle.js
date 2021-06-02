478f0fd324928e9c546bca5afd7769b1371ad04f
Ata-92 <atakal92@gmail.com>
2021-06-02 14:16:59 +0300

    Style Header container and h2

---
+++ src/components/header/HeaderStyle.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #00ADB5;
+  padding-bottom: 2vh;
+  /* height: 100vh; */
+`
+
+export const H2 = styled.h2`
+  font-size: xx-large;
+  margin-bottom: 1vh;
+`;
+
+
