4e7f51a5478736a8d10c4fae5c73b6ed06c23ef2
Ata-92 <atakal92@gmail.com>
2021-06-02 14:39:28 +0300

    Import comp., image and a hook

---
+++ src/components/login/Login.js
@@ -0,0 +1,12 @@
+import { Container, Wrapper, Img, H1, Input, Button } from './LoginStyle';
+import meal2 from '../../assets/meal2.svg';
+import { useHistory } from 'react-router-dom';
+
+const Login = ({login, setLogin}) => {
+  return (
+    <Container>
+    </Container>
+  )
+}
+
+export default Login;
