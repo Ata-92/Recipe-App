a9578dc134659ba2854200c9ecdf349452d83098
Ata-92 <atakal92@gmail.com>
2021-06-02 13:56:19 +0300

    Add an authentication in Route and Complete it

--- src/router/PrivateRouter.js
+++ src/router/PrivateRouter.js
@@ -2,6 +2,18 @@ import { Redirect, Route } from 'react-router-dom';
 
 const PrivateRouter = ({component: Component, login, ...rest}) => {
   return (
+    <Route
+      {...rest}
+      render={(props) => (
+        login ?
+        <Component {...props} />
+        :
+        <Redirect to={{
+          pathname: "/login",
+          state: {from: props.location}
+        }} />
+      )}
+    />
   )
 }
 
