8bc4e2946ee92410581d85229101b9ff2545cf3f
Ata-92 <atakal92@gmail.com>
2021-06-02 14:44:48 +0300

    Style Button and Complete Login component

--- src/components/login/LoginStyle.js
+++ src/components/login/LoginStyle.js
@@ -9,18 +9,18 @@ export const Container = styled.div`
 `;
 
 export const Wrapper = styled.div`
-  background-color: rgba(52, 154, 179, .7);
+  background-color: rgba(52, 154, 179, 0.7);
   width: 30%;
   margin: auto;
-  padding: 5vh .3vw;
-  border: .2vw solid #fff;
+  padding: 5vh 0.3vw;
+  border: 0.2vw solid #fff;
   border-radius: 50%;
-`
+`;
 
 export const H1 = styled.h1`
   color: white;
   margin: 1vh 0;
-`
+`;
 
 export const Img = styled.img`
   width: 10vw;
@@ -34,9 +34,22 @@ export const Input = styled.input`
   padding: 2vh 1vw;
   border: none;
   ::placeholder {
-    color: rgba(255, 255, 255, .5);
-    letter-spacing: .1vw;
+    color: rgba(255, 255, 255, 0.5);
+    letter-spacing: 0.1vw;
     text-align: left;
-    font-family: 'Times New Roman', Times, serif;
+    font-family: "Times New Roman", Times, serif;
   }
-`
+`;
+
+export const Button = styled.button`
+  cursor: pointer;
+  color: #fff;
+  background-color: rgb(52, 154, 179);
+  width: 15vw;
+  margin: 1vh 0;
+  padding: 2vh 1vw;
+  border: none;
+  font-weight: bolder;
+  font-size: large;
+  letter-spacing: 0.2vw;
+`;
