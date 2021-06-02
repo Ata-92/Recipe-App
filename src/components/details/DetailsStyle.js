0fec47c880d73ce457a63a02aadd61dd138fb8a1
Ata-92 <atakal92@gmail.com>
2021-06-02 14:30:34 +0300

    Style Wrapper and LeftInfo component

--- src/components/details/DetailsStyle.js
+++ src/components/details/DetailsStyle.js
@@ -27,3 +27,25 @@ export const H2 = styled.h2`
 `;
 
 export const Img = styled.img``;
+
+export const Wrapper = styled.div`
+  border: 0.2vw solid #fff;
+  border-radius: 0.vw;
+  margin: 0 3vw;
+  padding: 1vw 1vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const LeftInfo = styled.div`
+  float: left;
+  width: 40%;
+  box-sizing: border-box;
+  padding-right: 7vw;
+`;
+
+export const P = styled.p`
+  margin: 1vh 0;
+  text-align: right;
+`;
