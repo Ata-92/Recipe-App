553d7bf261839490a2cbadd183028e687c6b2aea
Ata-92 <atakal92@gmail.com>
2021-06-02 14:43:14 +0300

    Style Container, Wrapper comps.

---
+++ src/components/login/LoginStyle.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-image: url("https://picsum.photos/1600/900");
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+  background-color: rgba(52, 154, 179, .7);
+  width: 30%;
+  margin: auto;
+  padding: 5vh .3vw;
+  border: .2vw solid #fff;
+  border-radius: 50%;
+`
