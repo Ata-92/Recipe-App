e40d2c9672f063d4c1a2883e5c72e3ffbb7058a6
Ata-92 <atakal92@gmail.com>
2021-06-02 14:04:10 +0300

    Add RecipeCard and drill card object

---
+++ src/components/recipe/RecipeCard.js
@@ -0,0 +1,9 @@
+import { Card, H5, Img, Button } from "./MainStyle";
+import { useHistory } from 'react-router-dom';
+
+const RecipeCard = ({ card }) => {
+  return (
+  );
+};
+
+export default RecipeCard;
