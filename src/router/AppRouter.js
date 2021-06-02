c932d4e7e14f7a4d684490f02d49e8e514288e87
Ata-92 <atakal92@gmail.com>
2021-06-02 13:49:52 +0300

    Create hooks

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -8,3 +8,15 @@ import About from '../components/about/About';
 import Main from '../components/recipe/Main';
 import Details from '../components/details/Details';
 import PrivateRouter from './PrivateRouter';
+
+const AppRouter = () => {
+  const [login, setLogin] = useState(false);
+  const [cards, setCards] = useState();
+  const [query, setQuery] = useState();
+  const APP_ID = "e97f3f42";
+  const APP_KEY = "a4a3f97b1eb28df3474e7ce892e74e6d";
+  return (
+  )
+}
+
+export default AppRouter;
