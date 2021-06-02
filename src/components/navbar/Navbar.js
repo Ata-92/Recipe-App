79dc25385b477c2f15e2319e3c77d8236b8f47c8
Ata-92 <atakal92@gmail.com>
2021-06-02 14:36:08 +0300

    Add an event handler to toggle nav and icon

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,6 +4,11 @@ import { Container, Nav, Icon } from "./NavbarStyle";
 import { MdMenu } from 'react-icons/md';
 
 const Navbar = () => {
+  const [hamburger, setHamburger] = useState(false);
+
+  function handleClick() {
+    setHamburger(!hamburger);
+  }
 
   return (
     <Container>
