20333c4cc3ac537a02f118cb09556b70ecca1ee7
Ata-92 <atakal92@gmail.com>
2021-06-02 14:09:25 +0300

    Add and Complete Header.js

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import Form from "./Form";
+import { Container, H2 } from './HeaderStyle';
+
+const Header = () => {
+  return (
+    <Container>
+    <H2>Food App</H2>
+    <Form />
+    </Container>
+  )
+}
+
+export default Header;
