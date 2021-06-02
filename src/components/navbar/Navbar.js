c1c66181610214b60ff6c5ee9fdede209abce22c
Ata-92 <atakal92@gmail.com>
2021-06-02 14:32:24 +0300

    Import dependencies and add the func

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { Container, Nav, Icon } from "./NavbarStyle";
+import { MdMenu } from 'react-icons/md';
+
+const Navbar = () => {
+
+  return (
+    <Container>
+    </Container>
+  );
+};
+
+export default Navbar;
