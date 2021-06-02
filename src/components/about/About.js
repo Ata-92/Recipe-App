804d0289a9a15bd124865e586fe8e0eb3c3e8595
Ata-92 <atakal92@gmail.com>
2021-06-02 14:21:56 +0300

    Import components

---
+++ src/components/about/About.js
@@ -0,0 +1,11 @@
+import coding from '../../assets/coding.svg';
+import { Container, Img, Wrapper, H2, H3, H4 } from './AboutStyle';
+
+const About = () => {
+  return (
+    <Container>
+    </Container>
+  )
+}
+
+export default About;
